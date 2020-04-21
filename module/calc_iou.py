2013365e2f1d72393296b9a5d2fddd1b56197759
nope-pepepe <150442044@ccalumni.meijo-u.ac.jp>
2020-04-21 13:39:06 +0900

    Confusion MatrixからIoUを計算する関数(Pytorch向け)

---
+++ module/calc_iou.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#coding: utf-8
+
+import numpy as np
+import torch
+from tqdm import tqdm
+
+def calc_IoU(pred_label,gt_label, num_classes):
+    pred_label = pred_label.flatten()
+    gt_label = gt_label.flatten()
+    confusion = confusion_matrix(num_classes, pred_label, gt_label)
+    iou = confusion.diag() / confusion.sum(1)
+    #miou = torch.mean(iou)
+    
+    return iou
+
+def confusion_matrix(nb_classes, preds, label):
+    confusion_matrix = torch.zeros(nb_classes, nb_classes)
+    for t, p in zip(tqdm(label.view(-1), desc="confusion"), preds.view(-1)):
+        confusion_matrix[t.long(), p.long()] += 1
+
+    return confusion_matrix
