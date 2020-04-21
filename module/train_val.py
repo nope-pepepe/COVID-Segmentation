91444315eb2b08a4a71aeffee9717c0bbdf99be6
nope-pepepe <150442044@ccalumni.meijo-u.ac.jp>
2020-04-21 13:40:17 +0900

    Segmentation Taskに対応

--- module/train_val.py
+++ module/train_val.py
@@ -2,76 +2,53 @@
 # -*- Coding: utf-8 -*-
 
 import torch
+from tqdm import tqdm
+
+from module.calc_iou import calc_IoU
 
 def train(model, trainloader, optimizer, device, criterion, epoch, args):
     model.train()   #model訓練モードへ移行
-    correct = 0
-    total = 0
     running_loss = 0.0  #epoch毎の誤差合計
 
-    for i, (inputs, labels) in enumerate(trainloader):
+    for i, (inputs, labels) in enumerate(tqdm(trainloader, desc="train")):
         inputs, labels = inputs.to(device), labels.to(device)
         #出力計算
-        outputs = model(inputs)
+        outputs = model(inputs)["out"]
         loss = criterion(outputs, labels)
 
-        #Accuracy計算用素材
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-        #勾配計算とAdamStep
+        #勾配計算とOptimStep
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
     
-    accuracy = correct / total * 100.0
-    print("Train Epoch:{:>3} Acc:{:3.1f}% Loss:{:.4f}".format(epoch, accuracy, running_loss))
+    tqdm.write("Train Epoch:{:>3} Loss:{:.4f}".format(epoch, running_loss))
 
 def validation(model, valloader, device, criterion, args):
     model.eval()    #モデル推論モードへ移行
-    correct = 0
-    total = 0
     running_loss = 0.0  #epoch毎の誤差合計
 
     with torch.no_grad():   #勾配計算を行わない状態
-        for i, (inputs, labels) in enumerate(valloader):
+        for i, (inputs, labels) in enumerate(tqdm(valloader, desc="val")):
             inputs, labels = inputs.to(device), labels.to(device)
             #出力計算
-            outputs = model(inputs)
+            outputs = model(inputs)["out"]
             loss = criterion(outputs, labels)
 
-            #Accuracy計算用素材
+            #iou計算
             _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        
+            if i == 0:
+                preds = predicted
+                gt = labels
+            else:
+                preds = torch.cat([preds, predicted], dim=0)
+                gt = torch.cat([gt, labels], dim=0)
+
             running_loss += loss.item()
 
-        accuracy = correct / total * 100.0
-        print("Val Acc:{:3.1f}% Loss:{:.4f}".format(accuracy, running_loss))
+        iou = calc_IoU(preds, gt, num_classes=args.num_classes)
+        miou = torch.mean(iou)
 
-    return accuracy
+        tqdm.write("mIoU:{:3.1f}% Loss:{:.4f}".format(miou*100, running_loss))
 
-def test(model, device, dataloader, args, classes):
-    model.eval()
-
-    class_correct = list(0. for i in range(10))
-    class_total = list(0. for i in range(10))
-    with torch.no_grad():
-        for data in dataloader:
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs, 1)
-            c = (predicted == labels).squeeze()
-            for i in range(args.batchsize):
-                label = labels[i]
-                class_correct[label] += c[i].item()
-                class_total[label] += 1
-
-
-    for i in range(10): #各クラスのAccuracy表示
-        print('Accuracy of %5s : %2d %%' % (
-            classes[i], 100 * class_correct[i] / class_total[i]))
+    return iou, miou
