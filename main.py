a801afe97bbc71967cf382dfb7b497fbbb35cba8
nope-pepepe <150442044@ccalumni.meijo-u.ac.jp>
2020-04-22 15:37:45 +0900

    オプション部分を別ファイルへ

--- main.py
+++ main.py
@@ -24,26 +24,10 @@ from module.dataset import CovidDataset
 from module.get_model import get_model
 from module.classes import CLASSES
 from module.logger import LogIoU, logOption
+from module.opts import train_opts
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-g", "--gpu", type=int, default=0, help="使用GPU番号 空き状況はnvidia-smiで調べる")
-    parser.add_argument("-e", "--epoch", type=int, default=100, help="データセット周回数")
-    parser.add_argument("-b", "--batchsize", type=int, default=16, help="ミニバッチサイズ")
-    parser.add_argument("-lr", "--learningrate", type=float, default=0.001, help="学習率")
-    parser.add_argument("-m", "--model", type=str, default="Deeplab",
-                        choices=["Deeplab"])
-    parser.add_argument("-o", "--optimizer", type=str, default="SGD",
-                        choices=["SGD", "Adam"])
-    parser.add_argument("--scheduler", action="store_true", help="Use Scheduler")
-    parser.add_argument("-pre", "--pretrained", action="store_true", help="Use Pretrained model")
-    parser.add_argument("--step", type=int, default=10, help="schedulerのStep(何Epoch毎に減衰させるか)")
-    parser.add_argument("--num-worker", type=int, default=4, help="CPU同時稼働数 あまり気にしなくてよい")
-    parser.add_argument("--modelname", type=str, default="bestmodel.pth", help="保存モデル名")
-    parser.add_argument("--root_dir", type=str, default="dataset", help="データセットまでのパス")
-    parser.add_argument("--save_dir", type=str, default="results", help="データセットまでのパス")
-
-    args = parser.parse_args()
+    args = train_opts()
 
     device = torch.device("cuda:{}".format(args.gpu))   #GPUの設定
 
