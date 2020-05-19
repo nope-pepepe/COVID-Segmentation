bfe87a8a94457ab7dc34ea4cf1f6c4b7fddf6d79
nope-pepepe <150442044@ccalumni.meijo-u.ac.jp>
2020-05-19 15:31:57 +0900

    EfficientNetをバックボーンにしたDeeplabv3実装

--- module/get_model.py
+++ module/get_model.py
@@ -3,10 +3,16 @@
 
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 import torchvision
 from torchvision import models
+from torchvision.models.segmentation.deeplabv3 import DeepLabHead
+
+from collections import OrderedDict
 
 from module.unet import UNet
+
+from efficientnet_pytorch import EfficientNet
 
 def get_model(args, num_classes):
     if args.model == "Deeplab":
@@ -24,10 +30,80 @@ def get_model(args, num_classes):
             model = models.segmentation.deeplabv3_resnet101(
                 num_classes=num_classes
             )
+
     elif args.model == "UNet":
         model = UNet(1, num_classes, dropout=args.dropout)
+    
+    elif args.model == "EfficientDeeplab":
+        model = efficient_deeplabv3(args)
     
     else:
         print("no such a model")
         exit()
     return model
+
+class Deeplabv3(models.segmentation._utils._SimpleSegmentationModel):
+    def forward(self, x):
+        input_shape = x.shape[-2:]
+        features = self.backbone(x)
+
+        result = OrderedDict()
+        x = features
+        x = self.classifier(x)
+        x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+        result["out"] = x
+
+        if self.aux_classifier is not None:
+            x = features["aux"]
+            x = self.aux_classifier(x)
+            x = F.interpolate(x, size=input_shape, mode='bilinear', align_corners=False)
+            result["aux"] = x
+
+        return result
+
+class EfficientNetExtractor(EfficientNet):
+    def forward(self, inputs):
+        """use convolution layer to extract feature .
+        Args:
+            inputs (tensor): Input tensor.
+        Returns:
+            Output of the final convolution 
+            layer in the efficientnet model.
+        """
+        # Stem
+        x = self._swish(self._bn0(self._conv_stem(inputs)))
+
+        # Blocks
+        for idx, block in enumerate(self._blocks):
+            drop_connect_rate = self._global_params.drop_connect_rate
+            if drop_connect_rate:
+                drop_connect_rate *= float(idx) / len(self._blocks) # scale drop connect_rate
+            x = block(x, drop_connect_rate=drop_connect_rate)
+        
+        # Head
+        x = self._swish(self._bn1(self._conv_head(x)))
+
+        return x
+
+def efficient_deeplabv3(args):
+    # args.backbone には 'efficientnet-b0'などが入る
+    efficientnet = EfficientNetExtractor.from_name(args.backbone, in_channels=1)
+    #device = torch.device("cuda:{}".format(args.gpu))
+    #efficientnet = efficientnet.to(device)
+    
+    #backbone = efficientnet.extract_features
+    num_ch = efficientnet._conv_head.out_channels
+    classifier = DeepLabHead(num_ch, args.num_classes)
+    base_model = Deeplabv3
+    model = base_model(efficientnet, classifier)
+    return model
+
+if __name__ == "__main__":
+    class A:
+        pass
+    args = A() 
+    args.backbone = "efficientnet-b0"
+    args.num_classes = 4
+    model = efficient_deeplabv3(args)
+    img = torch.rand(2,1,512,512)
+    print(model(img)["out"].shape)
--- module/opts.py
+++ module/opts.py
@@ -10,14 +10,14 @@ def train_opts():
     parser.add_argument("-b", "--batchsize", type=int, default=4, help="ミニバッチサイズ")
     parser.add_argument("-lr", "--learningrate", type=float, default=0.001, help="学習率")
     parser.add_argument("-m", "--model", type=str, default="Deeplab",
-                        choices=["Deeplab", "UNet"])
+                        choices=["Deeplab", "UNet","EfficientDeeplab"])
     parser.add_argument("-o", "--optimizer", type=str, default="SGD",
                         choices=["SGD", "Adam"])
     parser.add_argument("-l", "--loss", type=str, default="CE",
                         choices=["CE", "focal"])
     parser.add_argument("--scheduler", action="store_true", help="Use Scheduler")
     parser.add_argument("-pre", "--pretrained", action="store_true", help="Use Pretrained model")
-    parser.add_argument("--weight", action="store_true", help="Use class weight")
+    parser.add_argument("--weight", action="store_false", help="Use class weight")
     parser.add_argument("--weight-softmax", action="store_true", help="Use class weight with softmax")
     parser.add_argument("--control-weight", action="store_true", help="Use class weight control to softmax value")
     parser.add_argument("--dropout", action="store_true", help="Use Dropout with UNet")
@@ -26,6 +26,7 @@ def train_opts():
     parser.add_argument("--modelname", type=str, default="bestmodel.pth", help="保存モデル名")
     parser.add_argument("--root_dir", type=str, default="dataset", help="データセットまでのパス")
     parser.add_argument("--save_dir", type=str, default="results", help="データセットまでのパス")
+    parser.add_argument("--backbone", type=str, default="efficientnet-b4", help="EfficientDeeplabのバックボーン選択")
 
     args = parser.parse_args()
 
@@ -36,7 +37,7 @@ def demo_opts():
     parser.add_argument("-g", "--gpu", type=int, default=0, help="使用GPU番号 空き状況はnvidia-smiで調べる")
     parser.add_argument("-b", "--batchsize", type=int, default=1, help="ミニバッチサイズ")
     parser.add_argument("-m", "--model", type=str, default="Deeplab",
-                        choices=["Deeplab", "UNet"])
+                        choices=["Deeplab", "UNet","EfficientDeeplab"])
     parser.add_argument("-r", "--readdir", type=str, default=None, help="読み出すディレクトリ名")
     parser.add_argument("--num-worker", type=int, default=4, help="CPU同時稼働数 あまり気にしなくてよい")
     parser.add_argument("--modelname", type=str, default="bestmodel.pth", help="保存モデル名")
